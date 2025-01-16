f00169ba76c20c7a1467c67837d8c1a6f249636b
Jens Brandenburg <jens@vragments.com>
2025-01-16 14:28:11 +0100

    camera position lerps

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,25 +1,18 @@
-import { Canvas, useThree } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Backdrop, Environment, Float, PerspectiveCamera } from '@react-three/drei';
+import { PerspectiveCamera as PerspectiveCameraImpl } from 'three';
+
 import HoverBoardAssembly from './three/HoverBoardAssembly';
 import { Color, Vector3 } from 'three';
 import { useZustand } from '../zustand';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const lookAtSceneRoot = new Vector3(0, 0, 0);
 
 const Scene = () => {
-    const cameraPosition = useZustand((store) => store.scene.cameraPosition);
-
     return (
         <Canvas shadows={true} gl={{ alpha: false, antialias: true }}>
-            <PerspectiveCamera
-                name='defaultCamera'
-                makeDefault
-                position={cameraPosition}
-                onUpdate={(self) => {
-                    self.lookAt(lookAtSceneRoot);
-                }}
-            />
+            <Camera />
 
             <Float speed={10} rotationIntensity={0} floatIntensity={1} floatingRange={[-0.01, 0.01]}>
                 <HoverBoardAssembly />
@@ -37,6 +30,22 @@ const Scene = () => {
 // useGLTF.preload(filePath);
 
 export default Scene;
+
+const Camera = () => {
+    const cameraPosition = useZustand((store) => store.scene.cameraPosition);
+    const cameraRef = useRef<PerspectiveCameraImpl | null>(null);
+
+    useFrame(() => {
+        if (cameraRef.current) {
+            if (cameraRef.current.position.distanceToSquared(cameraPosition) > 0.00001) {
+                cameraRef.current.position.lerp(cameraPosition, 0.1);
+                cameraRef.current.lookAt(lookAtSceneRoot);
+            }
+        }
+    });
+
+    return <PerspectiveCamera ref={cameraRef} name='defaultCamera' makeDefault />;
+};
 
 const Background = () => {
     const { preset, isVisible, color, showBackdrop } = useZustand((state) => state.settings.background);
