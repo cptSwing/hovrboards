e53ca6d5d0ea7c11edc6c5419b78cebe22398d30
Jens Brandenburg <jens@vragments.com>
2025-02-11 14:34:12 +0100

    toggleable strict

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+const useStrict = true;
+
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+    useStrict ? (
+        <StrictMode>
+            <App />
+        </StrictMode>
+    ) : (
         <App />
-    </StrictMode>,
+    ),
 );
